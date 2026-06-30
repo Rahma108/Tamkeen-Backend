6a6dbe0c0da5ffc46c25c7f9d90014a285a5a5e8
Rahma108 <salamarahma667@gmail.com>
2026-07-01 00:25:12 +0300

    Add Fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-// @ts-check
+import eslintConfigPrettier from 'eslint-config-prettier';
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
@@ -8,28 +8,52 @@ export default tseslint.config(
   {
     ignores: ['eslint.config.mjs'],
   },
+
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
+
+  ...tseslint.configs.recommendedTypeChecked({
+    tsconfigRootDir: import.meta.dirname,
+    project: ['./tsconfig.json'],
+  }),
+
   eslintPluginPrettierRecommended,
+
   {
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.jest,
       },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+      sourceType: 'module',
     },
   },
+
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        { checksVoidReturn: false },
+      ],
+
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
+
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+          singleQuote: true,
+          trailingComma: 'all',
+        },
+      ],
     },
   },
+
+  eslintConfigPrettier 
 );
